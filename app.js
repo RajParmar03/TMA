ae6d9b44351693eb249eb56eabf10112a1d8badf
RajParmar03 <rjbusiness03@gmail.com>
2024-04-25 22:56:38 +0530

    created restful routes for tasks and users

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 require('dotenv').config();
 
+const CONFIG = require("./config");
+const connection = require('./db');
+const taskRouter = require('./routes/task.routes');
+const userRouter = require('./routes/user.routes');
+
+const PORT = CONFIG.PORT;
+
 
 const app = express();
 
+app.use(express.json());
 
-const PORT = process.env.PORT || 8080;
-
-
-app.get("/" , (req , res) => {
+app.get('/', (req , res) => {
     res.status(200).send("Welcome to Task-Management-App");
-})
+});
 
-app.listen(PORT , () => {
+app.use('/tasks', taskRouter);
+app.use('/users', userRouter);
+
+app.listen(PORT , async() => {
+    try {
+        await connection;
+        console.log('DB connected');
+    } catch (error) {
+        console.log('DB connection failed : ' , error);
+    }
     console.log(`server is listening on ${PORT}`);
 })
